a8492037f076e436273bb0e213f21553200a8830
Boris Mudruk <velthers@gmail.com>
2020-05-14 12:57:21 +0300

    Adds asking name functionality

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+export const welcome = () => {
+    console.log('Welcome to the Brain Games!');
+}
+
+export const askName = () => {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
